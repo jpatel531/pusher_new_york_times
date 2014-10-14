a49ceaa6e119dec0c71ae6fbb0b5ba339e0bcb1a
Jamie Patel <jamie@notespublication.com>
2014-10-14 16:56:08 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-exports.articleSearchKey = "70f450fc8b8707259f719ba594394954:0:69999078"
-
-
-exports.pusherId = '92905'
-exports.pusherKey = '31649baed3ee58dc9afd'
-exports.pusherSecret = '70e9ca1d1406337268ad'
